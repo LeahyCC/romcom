3d75b94bf3c1aaf3c34f090d9a638ac860729587
Paige Vannelli <paigevannelli@gmail.com>
2020-12-09 19:44:41 -0700

    Complete iteration 0

--- src/main.js
+++ src/main.js
@@ -1,47 +1,38 @@
-// Create variables targetting the relevant DOM elements here 👇
-var coverImgSrc = document.querySelector(".cover-image");
-var title = document.querySelector(".cover-title");
+// ~~~~~~~~~~~~~~~~~ query selectors  ~~~~~~~~~~~~~~~~~~~~~~~~
+var coverImage = document.querySelector(".cover-image");
+var coverTitle = document.querySelector(".cover-title");
 // var tagline = document.querySelector(".tagline");
-var descriptor1 = document.querySelector(".tagline-1");
-var descriptor2 = document.querySelector(".tagline-2");
+var coverDescriptor1 = document.querySelector(".tagline-1");
+var coverDescriptor2 = document.querySelector(".tagline-2");
 
-// We've provided a few variables below
+// ~~~~~~~~~~~~~~~~~ global variables ~~~~~~~~~~~~~~~~~~~~~~~~
+
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 var currentCover;
 
-// Add your event listeners here 👇
-window.addEventListener("load", generateRandomCover);
+// ~~~~~~~~~~~~~~~~~ event listeners ~~~~~~~~~~~~~~~~~~~~~~~~
 
-// Create your event handlers and other functions here 👇
+window.addEventListener("load", displayCover);
 
+// ~~~~~~~~~~~~~~~~~ functions ~~~~~~~~~~~~~~~~~~~~~~~~
 
-// We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
 
 function generateRandomCover() {
-  var currentCover = new Cover(coverImgSrc, title, descriptor1, descriptor2)
-    currentCover.coverImgSrc = covers[getRandomIndex(covers)];
-    currentCover.title = titles[getRandomIndex(titles)];
-    currentCover.descriptor1 = descriptors[getRandomIndex(descriptors)];
-    currentCover.descriptor2 = descriptors[getRandomIndex(descriptors)];
+    currentCover = new Cover(covers[getRandomIndex(covers)],
+    titles[getRandomIndex(titles)],
+    descriptors[getRandomIndex(descriptors)],
+    descriptors[getRandomIndex(descriptors)])
 }
 
-// function displayCover() {
-// document.getElementById innerHTML
-// }
-
-// title.innerText = currentCover.title
-// descriptor1.innertext = currentCover.descriptor1
-// descriptor2.innertext = currentCover.descriptor2
-
-//It looks like we might need to change the html element for our photo? Need to double check the code :)
-
-// function generateTagline(descriptor1, descriptor2) {
-// `A tale of ${descriptor1} and ${descriptor2}`
-// }
-
-// coverImgSrc, title, descriptor1, descriptor2
+function displayCover() {
+  generateRandomCover();
+  coverImage.src = currentCover.cover;
+  coverTitle.innerText = currentCover.title;
+  coverDescriptor1.innerText = currentCover.tagline1;
+  coverDescriptor2.innerText = currentCover.tagline2;
+}
